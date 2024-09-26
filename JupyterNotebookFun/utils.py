ebd0df56e076915eba24826f2a21d2d9db7df9b0
Gina Sprint <sprint@gonzaga.edu>
2024-09-26 10:22:34 -0700

    discretization lab tasks

--- JupyterNotebookFun/utils.py
+++ JupyterNotebookFun/utils.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 def get_column(table, header, col_name):
@@ -36,3 +36,27 @@ def group_by(table, header, group_by_col_name):
         group_subtables[subtable_index].append(row)
 
     return unique_col_values, group_subtables
+
+def compute_equal_width_cutoffs(values, num_bins):
+    values_range = max(values) - min(values)
+    bin_width = values_range / num_bins
+    # bin_width is probably a float
+    # we will use numpy's version of range()
+    cutoffs = list(np.arange(min(values), max(values), bin_width))
+    cutoffs.append(max(values))
+    # optionally, round
+    cutoffs = [round(cutoff, 2) for cutoff in cutoffs]
+    return cutoffs
+
+def compute_bin_frequencies(values, cutoffs):
+    freqs = [0 for _ in range(len(cutoffs) - 1)] # because N + 1 cutoffs
+
+    for value in values:
+        if value == max(values):
+            freqs[-1] += 1 # add one to the last bin count
+        else:
+            for i in range(len(cutoffs) - 1):
+                if cutoffs[i] <= value < cutoffs[i + 1]:
+                    freqs[i] += 1 
+                    # add one to this bin defined by [cutoffs[i], cutoffs[i+1])
+    return freqs
